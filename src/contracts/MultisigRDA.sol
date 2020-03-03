1ed79fe415ea7ef06d7f9238b438dbe9252145b9
Matthias Nadler <matthias.nadler@unibas.ch>
2020-03-03 17:35:16 +0100

    add full multisig2of3 functionality with tests - finish implementation of multisig related code - add empty functions for the transaction logic - add extensive tests for all new functions - add npm run scripts for the individual test files - supress npm logs - update artifacts

--- src/contracts/MultisigRDA.sol
+++ src/contracts/MultisigRDA.sol
@@ -1,44 +1,268 @@
 pragma solidity >= 0.5.15  < 0.6.0;
 
 contract MultisigRDA {
-    address[3] private participants;
 
-//    enum TransactionType {ReturnDeposit, PayDamages, Migrate}
-    mapping (uint => Transaction) public transactions;
+    // --- Events ---
+    event Confirmation(address indexed sender, uint indexed txnId);
+    event Revocation(address indexed sender, uint indexed txnId);
+    event Submission(uint indexed txnId);
+    event Execution(uint indexed txnId);
+    event ExecutionFailure(uint indexed txnId);
+
+    // --- Storage ---
+    mapping(uint => Transaction) public transactions;
+    mapping(uint => mapping(address => bool)) public confirmations;
+    address[3] public participants;
+    mapping(address => bool) public isParticipant;
+    uint public txnCount;
+    enum TransactionType {ReturnDeposit, PayDamages, Migrate}
+
     struct Transaction {
         address owner;
-        uint8 tx_type;
-        address target;
+        TransactionType txnType;
+        address dest;
         bool executed;
         uint value;
     }
-    uint public transactionCount;
 
-    function addTransaction(uint8 tx_type, address target, uint value) public returns (uint transactionId){
-        transactionId = transactionCount;
-        transactions[transactionId] = Transaction({
-            owner: msg.sender,
-            tx_type: tx_type,
-            target: target,
-            executed: false,
-            value: value
-        });
-        transactionCount += 1;
+
+    // --- Modifiers ---
+    modifier onlyParticipant() {
+        require(isParticipant[msg.sender], 'RDA/not-allowed');
+        _;
     }
 
-    // temp, for testing
-    function getV(uint transactionId) public view returns(uint8 target) {
-        Transaction storage txn = transactions[transactionId];
-        target = txn.tx_type;
+    modifier transactionExists(uint txnId) {
+        require(transactions[txnId].owner != address(0), 'RDA/invalid-txn');
+        _;
     }
 
-    constructor(address one, address two, address three) public {
+    modifier confirmed(uint txnId, address participant) {
+        require(confirmations[txnId][participant], 'RDA/txn-not-confirmed');
+        _;
+    }
+
+    modifier notConfirmed(uint txnId, address participant) {
+        require(!confirmations[txnId][participant], 'RDA/txn-confirmed');
+        _;
+    }
+
+    modifier notExecuted(uint txnId) {
+        require(!transactions[txnId].executed, 'RDA/txn-executed');
+        _;
+    }
+
+    modifier transactionTypeExists(TransactionType txnType) {
+        require(uint(txnType) < 3, 'RDA/invalid-txn-type');
+        _;
+    }
+
+    // --- Functions ---
+
+    /// @dev Transaction to return the deposit. Takes no further arguments.
+    function submitTransaction(TransactionType txnType)
+        public
+        transactionTypeExists(txnType)
+        returns (uint txnId)
+    {
+        require(txnType == TransactionType.ReturnDeposit, 'RDA/invalid-arguments');
+        txnId = submitTransaction(txnType, address(0), 0);
+    }
+
+    /// @dev Transaction to pay the landlord.
+    function submitTransaction(TransactionType txnType, uint value)
+        public
+        returns (uint txnId)
+    {
+        require(txnType == TransactionType.PayDamages, 'RDA/invalid-arguments');
+        txnId = submitTransaction(txnType, address(0), value);
+    }
+
+    /// @dev Transaction to migrate the contract.
+    function submitTransaction(TransactionType txnType, address dest)
+        public
+        returns (uint txnId)
+    {
+        require(txnType == TransactionType.Migrate, 'RDA/invalid-arguments');
+        txnId = submitTransaction(txnType, dest, 0);
+    }
+
+    /// @dev INTERNAL transaction builder. Unifies all the overloaded functions
+    function submitTransaction(TransactionType txnType, address dest, uint value)
+        internal
+        returns (uint txnId)
+    {
+        txnId = addTransaction(txnType, dest, value);
+        confirmTransaction(txnId);
+    }
+
+    function confirmTransaction(uint txnId)
+        public
+        onlyParticipant
+        transactionExists(txnId)
+        notConfirmed(txnId, msg.sender)
+        notExecuted(txnId) // immutability of executed transactions
+    {
+        confirmations[txnId][msg.sender] = true;
+        emit Confirmation(msg.sender, txnId);
+    }
+
+    function revokeConfirmation(uint txnId)
+        public
+        onlyParticipant
+        transactionExists(txnId)
+        confirmed(txnId, msg.sender)
+        notExecuted(txnId)
+    {
+        confirmations[txnId][msg.sender] = false;
+        emit Revocation(msg.sender, txnId);
+    }
+
+    function executeTransaction(uint txnId)
+        public
+        onlyParticipant // this is conservative
+        transactionExists(txnId)
+        confirmed(txnId, msg.sender) // this is conservative
+        notExecuted(txnId)
+    {
+        if (isConfirmed(txnId)) {
+            Transaction storage txn = transactions[txnId];
+            txn.executed = true;
+            bool success = false;
+            if (txn.txnType == TransactionType.ReturnDeposit) {
+                success = returnDeposit();
+            } else if (txn.txnType == TransactionType.PayDamages) {
+                success = payDamages(txn.value);
+            } else if (txn.txnType == TransactionType.Migrate) {
+                success = migrate(txn.dest);
+            }
+            if (success) {
+                emit Execution(txnId);
+            } else {
+                emit ExecutionFailure(txnId);
+                txn.executed = false;
+            }
+        }
+    }
+
+    //noinspection NoReturn (will return false by default)
+    function isConfirmed(uint txnId)
+        public
+        view
+        returns (bool)
+    {
+        uint count = 0;
+        for (uint i = 0; i < 3; i++) {
+            if (confirmations[txnId][participants[i]])
+                count += 1;
+            if (count == 2)
+                return true;
+        }
+    }
+
+    function addTransaction(TransactionType txnType, address dest, uint value)
+        internal
+        onlyParticipant
+        returns (uint txnId)
+    {
+        txnId = txnCount;
+        transactions[txnId] = Transaction({
+            owner : msg.sender,
+            txnType : txnType,
+            dest : dest,
+            executed : false,
+            value : value
+            });
+        txnCount += 1;
+        emit Submission(txnId);
+    }
+
+    constructor(address one, address two, address three)
+        public
+    {
         participants = [one, two, three];
+        isParticipant[one] = true;
+        isParticipant[two] = true;
+        isParticipant[three] = true;
     }
 
-    // temp
-    function getAccs() public view  returns (address[3] memory) {
-        return participants;
+    // --- Executed Transactions ---
+
+    function returnDeposit()
+        internal
+        pure // temp
+        returns (bool success)
+    {
+        // will return deposit to the tenant
+        // function is not yet implemented
+        success = true;
+    }
+
+    function payDamages(uint value)
+        internal
+        pure // temp
+        returns (bool success)
+    {
+        // will transfer min(value, balance) DAI to the landlord
+        // function is not yet implemented and will throw compiler warnings (ignore them)
+        success = true;
+    }
+
+    function migrate(address dest)
+        internal
+        pure // temp
+        returns (bool success)
+    {
+        // will transfer all funds to a new address
+        // function is not yet implemented and will throw compiler warnings (ignore them)
+        success = true;
+    }
+
+    // --- Web3 call functions ---
+    function getConfirmationCount(uint txnId)
+        public
+        view
+        returns (uint count)
+    {
+        for (uint i = 0; i < 3; i++) {
+            if (confirmations[txnId][participants[i]]) {
+                count += 1;
+            }
+        }
+    }
+
+    function getConfirmationStatus(uint txnId)
+        public
+        view
+        returns (bool[] memory confirmationStatus)
+    {
+        confirmationStatus = new bool[](3);
+        for (uint i = 0; i < 3; i++) {
+            if (confirmations[txnId][participants[i]]) {
+                confirmationStatus[i] = true;
+            }
+        }
+    }
+
+    function getTransactionIds(bool pending, bool executed)
+        public
+        view
+        returns (uint[] memory txnIds)
+    {
+        uint[] memory txnIdsTemp = new uint[](txnCount);
+        uint count = 0;
+        uint i;
+        for (i = 0; i < txnCount; i++) {
+            if (pending && !transactions[i].executed
+            || executed && transactions[i].executed)
+            {
+                txnIdsTemp[count] = i;
+                count += 1;
+            }
+        }
+        txnIds = new uint[](count);
+        for (i = 0; i < count; i++)
+            txnIds[i] = txnIdsTemp[i];
     }
 
 }
--- src/test/3_Multisig.js
+++ src/test/3_Multisig.js
@@ -1,29 +1,324 @@
+const truffleAssert = require("truffle-assertions")
+
 const toWei = web3.utils.toWei
 const fromWei = web3.utils.fromWei
 
 const Multisig = artifacts.require("MultisigRDA")
 
-contract('Multisig', (accounts) => {
+contract("Multisig", (accounts) => {
+    let returnDepositId, payDamagesId, migrateId
+    const senders = [0,1,2] // accounts that add the transactions in this order
+
+    it(`check if contract is properly initialized`, async () => {
+        multisig = await Multisig.deployed()
+
+        assert.equal(await multisig.participants.call(0), accounts[senders[0]], "acc1 failed to init")
+        assert.equal(await multisig.participants.call(1), accounts[senders[1]], "acc2 failed to init")
+        assert.equal(await multisig.participants.call(2), accounts[senders[2]], "acc3 failed to init")
+
+        assert.equal(await multisig.isParticipant.call(accounts[senders[0]]), true)
+        assert.equal(await multisig.isParticipant.call(accounts[senders[1]]), true)
+        assert.equal(await multisig.isParticipant.call(accounts[senders[2]]), true)
+        assert.equal(await multisig.isParticipant.call(accounts[5]), false)
+    })
+
+    it(`add a "Return Deposit" transaction`, async () => {
+        const sender = senders[0]
+        const txnType = 0 // Return Deposit
+        multisig = await Multisig.deployed()
+
+        // access the correct overloaded function since javascript does not natively support function overloading
+        returnDepositId = await multisig.methods["submitTransaction(uint8)"].call(txnType, {from: accounts[sender]}) // get return value, no state change
+        const result = await multisig.methods["submitTransaction(uint8)"](txnType, {from: accounts[sender]}) // execute transaction
+
+        // assert submission and confirmation events
+        truffleAssert.eventEmitted(result, 'Submission', { txnId: returnDepositId })
+        truffleAssert.eventEmitted(result, 'Confirmation', { sender: accounts[sender], txnId: returnDepositId })
+
+        // retrieve the submitted transaction
+        const txn = await multisig.transactions.call(returnDepositId)
+        // console.log(txn)
+
+        assert.equal(txn.owner, accounts[sender], "owner mismatch")
+        assert.equal(txn.txnType, txnType, "TransactionType mismatch")
+        assert.equal(txn.dest, "0x0000000000000000000000000000000000000000", "destination mismatch")
+        assert.equal(txn.executed, false, "executed mismatch")
+        assert.equal(txn.value, 0, "value mismatch")
+
+        // check if the transaction is confirmed only by the sender
+        for (let i = 0; i < 3; i++) {
+            const confirm = await multisig.confirmations.call(returnDepositId, accounts[senders[i]])
+            assert.equal(confirm, i === sender, "wrong confirmation status")
+        }
+    })
+
+    it(`add a "Pay Damages" transaction`, async () => {
+        const sender = senders[1]
+        const txnType = 1 // Pay Damages
+        const amount = 50 // in DAI
+        multisig = await Multisig.deployed()
+
+        // access the correct overloaded function since javascript does not natively support function overloading
+        payDamagesId = await multisig.methods["submitTransaction(uint8,uint256)"].call(
+            txnType,
+            toWei(amount.toString(), "ether"),
+            {from: accounts[sender]}
+        )
+        const result = await multisig.methods["submitTransaction(uint8,uint256)"](
+            txnType,
+            toWei(amount.toString(), "ether"),
+            {from: accounts[sender]}
+        )
+
+        // assert submission and confirmation events
+        truffleAssert.eventEmitted(result, 'Submission', { txnId: payDamagesId })
+        truffleAssert.eventEmitted(result, 'Confirmation', { sender: accounts[sender], txnId: payDamagesId })
+
+        // retrieve the submitted transaction
+        const txn = await multisig.transactions.call(payDamagesId)
+
+        // assert the transaction
+        assert.equal(txn.owner, accounts[sender], "owner mismatch")
+        assert.equal(txn.txnType, txnType, "TransactionType mismatch")
+        assert.equal(txn.dest, "0x0000000000000000000000000000000000000000", "destination mismatch")
+        assert.equal(txn.executed, false, "executed mismatch")
+        assert.equal(fromWei(txn.value.toString(), "ether"), amount, "value mismatch")
+
+        // check if the transaction is confirmed only by the sender
+        for (let i = 0; i < 3; i++) {
+            const confirm = await multisig.confirmations.call(payDamagesId, accounts[senders[i]])
+            assert.equal(confirm, i === sender, "wrong confirmation status")
+        }
+    })
+
+    it(`add a "Migrate" transaction`, async () => {
+        const sender = senders[2]
+        const txnType = 2 // Migrate
+        const dest = accounts[9] // address of new contract or address to migrate to
+        multisig = await Multisig.deployed()
+
+        // access the correct overloaded function since javascript does not natively support function overloading
+        // First operation does not modify state and saves return value
+        migrateId = await multisig.methods["submitTransaction(uint8,address)"].call(
+            txnType,
+            dest,
+            {from: accounts[sender]}
+        )
+        // Second operation modifies state and returns the transaction hash
+        const result = await multisig.methods["submitTransaction(uint8,address)"](
+            txnType,
+            dest,
+            {from: accounts[sender]}
+        )
+
+        // assert submission and confirmation events
+        truffleAssert.eventEmitted(result, 'Submission', { txnId: migrateId })
+        truffleAssert.eventEmitted(result, 'Confirmation', { sender: accounts[sender], txnId: migrateId })
+
+        // retrieve the submitted transaction
+        const txn = await multisig.transactions.call(migrateId)
+
+        // assert the transaction
+        assert.equal(txn.owner, accounts[sender], "owner mismatch")
+        assert.equal(txn.txnType, txnType, "TransactionType mismatch")
+        assert.equal(txn.dest, dest, "destination mismatch")
+        assert.equal(txn.executed, false, "executed mismatch")
+        assert.equal(txn.value, 0, "value mismatch")
+
+        // check if the transaction is confirmed only by the sender
+        for (let i = 0; i < 3; i++) {
+            const confirm = await multisig.confirmations.call(migrateId, accounts[senders[i]])
+            assert.equal(confirm, i === sender, "wrong confirmation status")
+        }
+    })
+
+    it(`attempt to create each transaction type from a non-participant account`, async () => {
+        await truffleAssert.reverts(
+            multisig.methods["submitTransaction(uint8)"](0, {from: accounts[6]}),
+            "RDA/not-allowed",
+        )
+        await truffleAssert.reverts(
+            multisig.methods["submitTransaction(uint8,uint256)"](1, 100, {from: accounts[7]}),
+            "RDA/not-allowed",
+        )
+        await truffleAssert.reverts(
+            multisig.methods["submitTransaction(uint8,address)"](2, accounts[5], {from: accounts[8]}),
+            "RDA/not-allowed",
+        )
+        // validate that there are 3 transactions
+        assert.equal((await multisig.getTransactionIds.call(true, false)).length, 3, 'wrong number of pending transactions')
+
+    })
+
+    it(`attempt to create transactions with invalid data`, async () => {
+        // wrong function for event types
+        await truffleAssert.reverts(
+            multisig.methods["submitTransaction(uint8)"](1, {from: accounts[senders[0]]}),
+            "RDA/invalid-arguments",
+        )
+        await truffleAssert.reverts(
+            multisig.methods["submitTransaction(uint8,uint256)"](2, 100, {from: accounts[senders[0]]}),
+            "RDA/invalid-arguments",
+        )
+        await truffleAssert.reverts(
+            multisig.methods["submitTransaction(uint8,address)"](0, accounts[6], {from: accounts[senders[0]]}),
+            "RDA/invalid-arguments",
+        )
+        // invalid event type
+        await truffleAssert.fails(
+            multisig.methods["submitTransaction(uint8)"](4, {from: accounts[senders[0]]}),
+            "status 0",
+        )
+    })
+
+    it(`revoke confirmation for "Return Deposit" transaction`, async () => {
+        // Revoke for unauthorized participant
+        await truffleAssert.reverts(
+            multisig.revokeConfirmation(returnDepositId, {from: accounts[4]}),
+            "RDA/not-allowed",
+        )
+
+        // Revoke for a participant that did not confirm yet
+        await truffleAssert.reverts(
+            multisig.revokeConfirmation(returnDepositId, {from: accounts[senders[2]]}),
+            "RDA/txn-not-confirmed",
+        )
+
+        // Revoke for a participant that did confirm
+        const result = await multisig.revokeConfirmation(returnDepositId, {from: accounts[senders[0]]})
+        truffleAssert.eventEmitted(result, 'Revocation', { sender: accounts[senders[0]], txnId: returnDepositId })
+
+        // transaction should have 0 confirmations
+        for (let i = 0; i < 3; i++) {
+            const confirm = await multisig.confirmations.call(returnDepositId, accounts[senders[i]])
+            assert.equal(confirm, false, "wrong confirmation status")
+        }
+        assert.equal(await multisig.getConfirmationCount(returnDepositId), 0, 'wrong number of confirmations')
+    })
+
+    it(`add second confirmation for "Pay Damages" transaction`, async () => {
+        // Add for unauthorized participant
+        await truffleAssert.reverts(
+            multisig.confirmTransaction(payDamagesId, {from: accounts[7]}),
+            "RDA/not-allowed",
+        )
+
+        // Add for a participant that did already add
+        await truffleAssert.reverts(
+            multisig.confirmTransaction(payDamagesId, {from: accounts[senders[1]]}),
+            "RDA/txn-confirmed",
+        )
+
+        // verify isConfirmed == false
+        assert.equal(await multisig.isConfirmed.call(payDamagesId), false, 'transaction should be unconfirmed')
 
 
-  it(`check if contract is properly initialized`, async () => {
-    multisig = await Multisig.deployed()
-    const accs = await multisig.getAccs.call()
-    assert.equal(accs[0], accounts[0], 'acc1 failed to init')
-    assert.equal(accs[1], accounts[1], 'acc2 failed to init')
-    assert.equal(accs[2], accounts[2], 'acc3 failed to init')
-  })
+        // Add for a participant that did not confirm yet
+        const result = await multisig.confirmTransaction(payDamagesId, {from: accounts[senders[0]]})
+        truffleAssert.eventEmitted(result, 'Confirmation', { sender: accounts[senders[0]], txnId: payDamagesId })
+        // transaction should have 2 confirmations
+        for (let i = 0; i < 3; i++) {
+            const confirm = await multisig.confirmations.call(payDamagesId, accounts[senders[i]])
+            assert.equal(confirm, [0,1].includes(i), "wrong confirmation status")
+        }
+        assert.equal(await multisig.getConfirmationCount(payDamagesId), 2, 'wrong number of confirmations')
+        // verify isConfirmed == true
+        assert.equal(await multisig.isConfirmed.call(payDamagesId), true, 'transaction should be confirmed')
+        // verify addresses and test getConfirmationStatus function
+        const status = await multisig.getConfirmationStatus.call(payDamagesId)
+        assert.equal(status[0], true, 'confirmation status mismatch')
+        assert.equal(status[1], true, 'confirmation status mismatch')
+        assert.equal(status[2], false, 'confirmation status mismatch')
+    })
 
- it(`add a transaction`, async () => {
-   multisig = await Multisig.deployed()
-   const id = await multisig.addTransaction.call(7, accounts[4], 100)
-   await multisig.addTransaction(7, accounts[4], 100)
-   // console.log("ID", id)
+    it(`remove the added confirmation for "Pay Damages" and add it again`, async () => {
+        // Revoke for participant that did confirm
+        let result = await multisig.revokeConfirmation(payDamagesId, {from: accounts[senders[0]]})
+        truffleAssert.eventEmitted(result, 'Revocation', { sender: accounts[senders[0]], txnId: payDamagesId })
+        // verify isConfirmed == false
+        assert.equal(await multisig.isConfirmed.call(payDamagesId), false, 'transaction should be unconfirmed')
+        assert.equal(await multisig.getConfirmationCount(payDamagesId), 1, 'wrong number of confirmations')
 
-   const transaction = await multisig.getV.call(id)
-   // console.log(transaction.toString())
+        // Add again
+        result = await multisig.confirmTransaction(payDamagesId, {from: accounts[senders[0]]})
+        truffleAssert.eventEmitted(result, 'Confirmation', { sender: accounts[senders[0]], txnId: payDamagesId })
+        // verify isConfirmed == true
+        assert.equal(await multisig.isConfirmed.call(payDamagesId), true, 'transaction should be confirmed')
+        assert.equal(await multisig.getConfirmationCount(payDamagesId), 2, 'wrong number of confirmations')
+    })
 
-   assert.equal(transaction, 7, 'Transaction not properly added')
- })
+    it(`add third confirmation and check if still confirmed then remove it`, async () => {
+        // Add third confirmation
+        let result = await multisig.confirmTransaction(payDamagesId, {from: accounts[senders[2]]})
+        truffleAssert.eventEmitted(result, 'Confirmation', { sender: accounts[senders[2]], txnId: payDamagesId })
+        // transaction should have 3 confirmations
+        for (let i = 0; i < 3; i++) {
+            const confirm = await multisig.confirmations.call(payDamagesId, accounts[senders[i]])
+            assert.equal(confirm, true, "wrong confirmation status")
+        }
+        assert.equal(await multisig.getConfirmationCount(payDamagesId), 3, 'wrong number of confirmations')
+        // verify isConfirmed == true
+        assert.equal(await multisig.isConfirmed.call(payDamagesId), true, 'transaction should be confirmed')
+        // Revoke again
+        result = await multisig.revokeConfirmation(payDamagesId, {from: accounts[senders[2]]})
+        truffleAssert.eventEmitted(result, 'Revocation', { sender: accounts[senders[2]], txnId: payDamagesId })
+        assert.equal(await multisig.getConfirmationCount(payDamagesId), 2, 'wrong number of confirmations')
+        // verify isConfirmed == true
+        assert.equal(await multisig.isConfirmed.call(payDamagesId), true, 'transaction should be confirmed')
+    })
+
+    it(`execute transaction "Pay Damages" with invalid and valid participant`, async () => {
+        // invalid participant
+        await truffleAssert.reverts(
+            multisig.executeTransaction(payDamagesId, {from: accounts[8]}),
+            "RDA/not-allowed",
+        )
+        // valid participant
+        const result = await multisig.executeTransaction(payDamagesId, {from: accounts[senders[1]]})
+        truffleAssert.eventEmitted(result, 'Execution', { txnId: payDamagesId })
+        const txn = await multisig.transactions.call(payDamagesId)
+        assert.equal(txn.executed, true, 'transaction should be executed')
+        // test if pending and executed transactionIds (or counts) match
+        assert.equal((await multisig.getTransactionIds.call(true, false)).length, 2, 'wrong number of pending transactions')
+        assert.equal((await multisig.getTransactionIds.call(false, true))[0].toString(), payDamagesId.toString(), 'wrong number of pending transactions')
+    })
+
+    it(`try to revoke and add confirmation on an executed transaction`, async () => {
+        // revoke confirmation
+        await truffleAssert.reverts(
+            multisig.revokeConfirmation(payDamagesId, {from: accounts[senders[1]]}),
+            "RDA/txn-executed",
+        )
+        // add confirmation
+        await truffleAssert.reverts(
+            multisig.confirmTransaction(payDamagesId, {from: accounts[senders[2]]}),
+            "RDA/txn-executed",
+        )
+    })
+
+    it(`confirm and execute the "Return Deposit" and "Migrate" transaction`, async () => {
+        // Return Deposit
+        for (let i = 0; i < 2; i++) {
+            let result = await multisig.confirmTransaction(returnDepositId, {from: accounts[senders[i]]})
+            truffleAssert.eventEmitted(result, 'Confirmation', { sender: accounts[senders[i]], txnId: returnDepositId })
+        }
+
+        let result = await multisig.executeTransaction(returnDepositId, {from: accounts[senders[0]]})
+        truffleAssert.eventEmitted(result, 'Execution', { txnId: returnDepositId })
+        let txn = await multisig.transactions.call(returnDepositId)
+        assert.equal(txn.executed, true, 'transaction should be executed')
+
+        // Migrate
+        result = await multisig.confirmTransaction(migrateId, {from: accounts[senders[1]]})
+        truffleAssert.eventEmitted(result, 'Confirmation', { sender: accounts[senders[1]], txnId: migrateId })
+
+        result = await multisig.executeTransaction(migrateId, {from: accounts[senders[2]]})
+        truffleAssert.eventEmitted(result, 'Execution', { txnId: migrateId })
+        txn = await multisig.transactions.call(migrateId)
+        assert.equal(txn.executed, true, 'transaction should be executed')
+
+        assert.equal((await multisig.getTransactionIds.call(true, false)).length, 0, 'wrong number of pending transactions')
+        assert.equal((await multisig.getTransactionIds.call(false, true)).length, 3, 'wrong number of pending transactions')
+    })
 
 })
